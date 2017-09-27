eaaafbbddbabb2d912f51144189eada99a5e7bf9
Sohail Haider <sohailh343@gmail.com>
2017-09-27 13:35:55 +0500

    ChunkTree Inherited

--- AIFBS_Test.cpp
+++ AIFBS_Test.cpp
@@ -1,9 +1,29 @@
-#include "AIFBS_Includes.hpp"
+#include "AIFBS_ChunkTreeNode.cpp"
+#include "AIFBS_ChunkTree.cpp"
 #include <iostream>
 
 using namespace std;
 using namespace AIFBS;
 int main() {
-	
+	AIFBS_ChunkTree t(3);
+	t.insert(10);
+	t.insert(20);
+	t.insert(5);
+	t.insert(6);
+	t.insert(12);
+	t.insert(30);
+	t.insert(7);
+	t.insert(17);
+
+	cout << "Traversal of the constucted tree is ";
+	t.traverse();
+
+	int k = 6;
+	(t.search(k) != NULL)? cout << "\nPresent" : cout << "\nNot Present\n";
+
+	k = 15;
+	(t.search(k) != NULL)? cout << "\nPresent" : cout << "\nNot Present\n";
+
+	return 0;
 }
 
